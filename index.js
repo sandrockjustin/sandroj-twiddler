0d1674ea9c87160b56054ab277e450adda1a71cc
Justin Sandrock <sandrock.justin@gmail.com>
2024-08-08 15:14:26 -0500

    attempting to add moment() library

--- index.js
+++ index.js
@@ -3,14 +3,23 @@ $(document).ready(() => {
   const $body = $('body');
   $body.html('');
 
+  // for each item in streams.home (array) containing all posts on the entire website
   const $tweets = streams.home.map((tweet) => {
-    const $tweet = $('<div></div>');
-    const text = `@${tweet.user}: ${tweet.message}`;
 
+    // create a new division for each tweet
+    const $tweet = $('<div></div>');
+
+    // create a variable (String) that uses the current tweet properties (.user & .message & .created_at) 
+    const text = `@${tweet.user}: ${tweet.message} ${moment().startOf(tweet.created_at).fromNow()}`;
+
+    // add the variable 'text' (String) to our newly created division
     $tweet.text(text);
 
+    // return the new division
     return $tweet;
   });
+
+  // appends result from running $tweets
   $body.append($tweets);
 
 });
