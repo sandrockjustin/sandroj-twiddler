1b18d93f2cbbd5749f9cf717b202f2dc580a43d8
Justin Sandrock <sandrock.justin@gmail.com>
2024-08-09 10:38:54 -0500

    minimum requirements almost fulfilled, developing ability for user to create posts

--- index.js
+++ index.js
@@ -3,48 +3,63 @@ $(document).ready(() => {
   const $body = $('body');
   $body.html('');
 
-
-  // $tweets is NOT a function, it just stores a result from map(), it is an array!
-  // for each item in streams.home (array) containing all posts on the entire website
-  const $tweets = streams.home.map((tweet) => {
-
-    const $tweet = $('<div></div>'); // create a new division for each tweet
-
-    // create a variable (String) that uses the current tweet properties (.user & .message & .created_at)
-    const text = `@${tweet.user}: ${tweet.message} ${moment(tweet.created_at).startOf('second').fromNow()}`;
-
-    $tweet.text(text); // add the variable 'text' (String) to the text of our newly created division
-
-    return $tweet;    // return the new division
-  });
-
-  $body.append($tweets); // appends result from running $tweets
-
-  scheduleNextTweet(); // allows for new items to be generated
-
-  // update() does verything that $tweets does, but it iterates continuously
   function update(){
-    $body.html(''); // clean document
-
-    // get an updated list of posts from stream.home (array)
-    const $updateTweets = streams.home.map((tweet) => {
-
-      const $tweet = $('<div></div>'); // create a new division for each tweet
-
-      // create a variable (String) that uses the current tweet properties (.user & .message & .created_at)
-      const text = `@${tweet.user}: ${tweet.message} ${moment(tweet.created_at).startOf('second').fromNow()}`;
-
-      $tweet.text(text); // add the variable 'text' (String) to the text of our newly created division
-
-      return $tweet;    // return the new division
+    $body.html(''); // clean document, wipes page at start
+    const $updateTweets = streams.home.map((tweet) => { // get an updated list of posts from stream.home (array)
+      const $division = $('<div class="tweet-container"></div>'); // create a new division for each tweet
+      $division.append(`<table class="table-main"><tr class="table-header"><th class="tweet-username">@${tweet.user}</th></tr><tr class="table-body"><td class="tweet-contents">${tweet.message}</td></tr><tr class="table-footer"><td class="tweet-timestamp">${moment().format('lll')} - <i>${moment(tweet.created_at).startOf('second').fromNow()}</i></td></tr></table>`)
+      return $division; // return value of division
     });
 
-    // add the REVERSE of $updateTweets (newest results are pushed to end) and append that to the cleaned document
-    $body.append($updateTweets.reverse());
-
-    setInterval(update, 15000);
+    $body.append($updateTweets.reverse()); // add the REVERSE of $updateTweets (newest results are pushed to end)
+    $body.prepend('<button class="visitor-createTweet">+</button>'); // allows user to write a tweet
+    $body.prepend('<button class="refresh">&#x21bb;</button>'); // allows user to refresh feed
   }
+
+  $(document).on('click', '.refresh', function(event){
+    update();
+    console.log('page refreshed');
+  })
+
+  // for all items of class tweet-username, when they are clicked
+  $(document).on('click', '.tweet-username', function(event){
+    let $target = event.currentTarget.innerText.slice(1);           // $target represents the item that was clicked
+    let $userTweets = streams.home.filter(function(currentTweet){   // for each Tweet on the website
+      if (currentTweet.user === $target){                           // if the Tweet belongs to the event target
+        return currentTweet;                                        // push it to an array, which gets returned on line 31
+      }
+    }).map(function(tweet){                                         // from array produced from filter()
+      const $division = $('<div class="tweet-container"></div>');   // create a new $division that will receive modifications
+      const $tableHead = `<tr class="table-header"><th class="tweet-username">@${tweet.user}</th></tr>`;
+      const $tableBody = `<tr class="table-body"><td class="tweet-contents">${tweet.message}</td></tr>`;
+      const $tableFoot = `<tr class="table-footer"><td class="tweet-timestamp">${moment().format('lll')} - <i>${moment(tweet.created_at).startOf('second').fromNow()}</i></td></tr>`;
+      $division.prepend(`<table class="table-main">${$tableHead}${$tableBody}${$tableFoot}</table>`)
+      return $division;                                             // return that $division so that we can append to $body
+    });
+
+    $body.html('');
+    $body.append($userTweets);
+    $body.prepend('<button class="visitor-createTweet">+</button>'); // allows user to write a tweet
+    $body.prepend('<button class="refresh">&#x21bb;</button>'); // allows user to refresh feed
+
+  })
+
+  $(document).on('click', '.visitor-createTweet', function(event){
+    update();
+    $division = $('<div></div>')
+    $division.append(`<form><input type="text" name="message"><button type="submit" class="visitor-submitTweet">Post</button></form>`)
+    $body.prepend($division);
+    console.log('waiting for user input...')
+  })
+
+  $(document).on('click', '.visitor-submitTweet', function(){
+    //let userInput = $('input').val();
+    console.log('hello world');
+  })
+
   update();
+
+  scheduleNextTweet(); // allows for new items to be generated
 
 });
 
